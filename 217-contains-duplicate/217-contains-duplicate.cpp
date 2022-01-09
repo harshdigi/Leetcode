ab64f39aca028fdfd1705053e6a8f234bcc3ae21
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-01-09 16:28:50 +0530

    Time: 100 ms (29.14%), Space: 53.5 MB (9.78%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+     int n = nums.size();
         
-        for(int i =0;i<nums.size()-1;i++)
-        {
-            if(nums[i]==nums[i+1]){
-                return true;
-            }
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i < n; i++){
             
+            mp[nums[i]]++;
+        }
+        
+        for(auto x : mp){
+            
+            if(x.second > 1)
+                return true;
         }
         
         return false;
