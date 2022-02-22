73a95c2509fed712e2bb8bcbed3358b37c5acd86
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-02-22 13:50:14 +0530

    Time: 4 ms (96.11%), Space: 7.2 MB (67.31%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int index=-1,j=0,i=0;
-        int hL=haystack.length(),nL= needle.length();
-        if(nL>hL){
-            return -1;
-        }
-        if(nL==0){
-            return 0;
-        }
-        while(i<hL){
-           
-            if(haystack[i]== needle[j]){
-                if(j==0){
-                    index =i;
-                }
-                j++;
-                i++;
-                if (j==nL){
-                return index;}
-            }
-            else if(index!=-1){
-                i=index+1;
-                index=-1;
-                j=0;
-            }
-            else if(index==-1 and nL>hL-i ){
-                        break;
-            }
-           
-            else{
-                i++;
-            }
-            
-            
-        }
-        return -1;
+    int n = haystack.length();
+    int m = needle.length();
+     if(m==0){    
+         return 0;
+     }
+     if(m>n){    
+         return -1;
+     }
+     int k = 0;    
+     int i = 0;    
+     int flag = 0;   
+     int index = 0;  
+ 	
+     while(i<n){
+         if(haystack[i]==needle[k] and i+m-1<n and haystack[i+m-1] == needle[m-1] and flag== 0 || haystack[i]==needle[k] and flag==1){
+             if(flag==0){
+                 index = i;
+             }
+             flag = 1;
+             i++;
+             k++;
+             if(k>=m){
+                 return index;
+             }
+             continue;
+         }
+     
+         if (haystack[i]!=needle[k] and flag==1){
+             k=0;
+             flag=0;
+             i = index+1;
+             index = 0;
+             continue;
+         }
+         i++;
+     }
+     return -1;
+
     }
 };
