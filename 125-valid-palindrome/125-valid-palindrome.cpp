27fd23550184a22e295a6a8ddc204d824e1c34a2
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-02-25 21:40:13 +0530

    Time: 8 ms (57.94%), Space: 7.5 MB (52.54%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -4,10 +4,11 @@ public:
       int low=0,high=s.length()-1;
         transform(s.begin(), s.end(), s.begin(), ::tolower);
         while(low<high){
-            if( !((s[low] >=97 && s[low]<=122) || (s[low] >=48 && s[low]<=57)) ){
+            if( !((s[low] >='a' && s[low]<='z') || (s[low] >='0' && s[low]<='9')) ){
                 low++;
             }
-            else if( !( (s[high] >=97 && s[high]<=122) || (s[high] >=48 && s[high]<=57) ) ){
+            else if( !( (s[high] >='a' && s[high]<='z') || (s[high] >='0' && s[high]<='9')) ) 
+            {
                 high--;
             }
             
