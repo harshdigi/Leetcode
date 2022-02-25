95c7421a83466384dd362088a8b536cdc5c83c52
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-02-25 20:37:23 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (34.04%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -6,10 +6,11 @@ public:
     if (numRows==0) return ans;
         ans.push_back({1});
         for(int i=1;i<numRows;i++){
+            vector<int> pRow= ans[i-1];
             vector<int> row ={1};
             for(int j=1;j<i;j++){
                 
-                row.push_back(ans[i-1][j-1]+ans[i-1][j]);
+                row.push_back(pRow[j-1]+pRow[j]);
                 
             }
             row.push_back(1);
