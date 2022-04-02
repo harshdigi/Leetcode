c8d3c55c3eceb5ecc31707145f0679ed7786b689
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-04-02 17:59:10 +0530

    Time: 3 ms (86.07%), Space: 8.9 MB (50.58%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -26,6 +26,17 @@ public:
         }
         left++;
         right--;
-        return findPeak(nums, left, right);
+        while(left<right){
+int mid = left+(right-left)/2;
+
+        if(nums[mid]<nums[mid+1]){
+            left=mid+1;
+        }
+        else{
+            right = mid;
+        }
+    }
+    return left;
+
     }
 };
