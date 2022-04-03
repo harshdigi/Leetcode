9f52a45a351e4966f26184e926f2b49f16165e0e
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-04-03 23:06:35 +0530

    Time: 0 ms (100.00%), Space: 6 MB (17.04%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        double y=pow(num,0.5);
-        if(fmod(num,y)==0){
-            return true;
+        long long int low = 0;
+        long long int high = num;
+        long long int sq;
+        
+        while(low <= high)
+        {
+            long long int mid = low + (high - low)/2;
+            sq = mid*mid;
+            if(sq == num)
+                return true;
+            else if(sq > num)
+                high = mid-1;
+            else 
+                low = mid+1;
         }
         return false;
     }
