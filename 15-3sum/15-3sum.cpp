06bee1fcef76383a99e36d906cec4e58d06dfaf1
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-04-03 22:46:23 +0530

    Time: 62 ms (95.79%), Space: 20 MB (86.93%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,61 +1,39 @@
 class Solution {
-private:
-bool allEqual(std::vector<int> const &v) {
-    if (v.size() == 0) {
-        return false;
-    }
- 
-    return std::all_of(v.begin(), v.end(), [&] (int const &e) {
-        return e == 0;
-    });
-}
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    
+public:     
+vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        if(allEqual(nums)==true && nums.size()>2){
-            vector<int> x = {0,0,0};
-            ans.push_back(x);
-            return ans;
-        }
-        int a = count(nums.begin() , nums.end() , 0);
-        int b = count(nums.begin() , nums.end() , -1);
-        int c = count(nums.begin() , nums.end() , 1);
-        if(a>0 && b>0 && c>0 && (a+b+c == nums.size())){
-            vector<int> x = {-1 , 0 , 1};
-            vector<int> y = {0 , 0 , 0};
-            ans.push_back(x);
-            if(a>2){
-            ans.push_back(y);
-            }
-            return ans;
-        }
-        
-        sort(nums.begin() , nums.end());
-        for(int i=0 ; i<nums.size() ; i++){
-            int j=i+1;
-            int k=nums.size()-1;
-            int target = 0 - nums[i];
-            while(j<k){
-                vector<int> v;
-                if((nums[j]+nums[k])==target){
-                    v.push_back(nums[i]);
-                    v.push_back(nums[j]);
-                    v.push_back(nums[k]);
-                    ans.push_back(v);
-  
-                    j++;
-                    k--;
-                }
-                else if((nums[j]+nums[k])<target){
-                    j++;
-                }
-                else{
-                    k--;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            else{
+                int rem=-1*nums[i];
+                int p1=i+1;
+                int p2=n-1;
+                while(p1<p2){
+                    int curr=nums[p1]+nums[p2];
+                    if(curr>rem)
+                        p2--;
+                    else if(curr<rem)
+                        p1++;
+                    else{
+                        ans.push_back({nums[i],nums[p1],nums[p2]});
+                        if(nums[p1]==nums[p2])
+                            break;
+                        else{
+                            int x=nums[p1];
+                            int y=nums[p2];
+                            while(nums[p1]==x)
+                                p1++;
+                            while(nums[p2]==y)
+                                p2--;
+                        }
+                    }
                 }
             }
         }
-        sort(ans.begin() , ans.end());
-        ans.erase(std::unique(ans.begin(), ans.end()), ans.end());
         return ans;
-      }
+    }
 };
