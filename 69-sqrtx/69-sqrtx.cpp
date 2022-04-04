8a3d251b58e560e045651dceaf1e2c1d19df646c
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-04-04 23:34:12 +0530

    Time: 4 ms (57.41%), Space: 6 MB (26.85%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     int mySqrt(int x) {
-     int lo, hi;
-        long long int mid;
-        
-        lo=1, hi = x;
-        while(lo < hi){
-            mid = lo+ (hi- lo +1)/2;
-            
-            if((mid * mid) > x){
-                hi = mid - 1;
+        int s=0;
+        int e=x;
+
+        int ans=0;
+
+        while(s<=e)
+        {
+            long long int mid=(s+e)/2;
+
+            if(mid*mid==x)
+            {
+                ans=mid;
+                return ans;
             }
-            else lo = mid;
-            
+
+            if(mid*mid<x)
+            {
+                ans=mid;
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
+            }
+
         }
-        //sanity check
-        if((lo * lo) <= x)    return lo;
-        else return 0;
-    
+
+        return ans;
     }
 };
