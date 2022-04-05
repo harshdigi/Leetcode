6a6adc6812bea277afdb0833d676f46990323716
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-04-05 19:33:11 +0530

    Time: 25 ms (42.48%), Space: 8.7 MB (38.06%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+         int sizeS = s.length();
+        int sizeP = p.length();
+        if(sizeP>sizeS){
+            return {};
+        }
+        vector<int> hashS(26,0);
+        vector<int> hashP(26,0);
+        
+        for(int i=0;i<sizeP;i++){
+            hashS[s[i] - 'a']++;
+            hashP[p[i] - 'a']++;
+        }
+        vector<int> ans;
+        
+        int i=0;
+        if(hashS==hashP){
+            ans.push_back(0);
+        }
+        for(int j=sizeP;j<sizeS;){
+            hashS[s[j]-'a']++;
+            hashS[s[i]-'a']--;
+            i++;
+            j++;
+            
+            if(hashS==hashP){
+                ans.push_back(i);
+            }
+           
+            
+        }
+        return ans;
+    }
+};
