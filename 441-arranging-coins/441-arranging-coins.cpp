b9a508fe95de61cc440ba36705e87e9bc9f76cf6
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-04-06 19:03:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (28.00%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int ans =0;
-        int currentR=1;
-        while(currentR<=n){
-            n=n-currentR;
-            currentR++;
-            ans++;
+      
+        
+        if(n==1){
+            return 1;
         }
-        return ans;
+        long long int left =0, right = n-1,mid,val;
+        while(left<=right){
+            mid = left+(right-left)/2;
+            val = mid*(mid+1)/2; // AP sum of n consecutive number 1,2,3,4,5....n = (n*(n+1))/2
+            if(val==n){
+                return mid;
+            }
+            else if(val>n){
+                right =mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        
+        return right;
+        
+           
         
     }
 };
