ec9c0f7d395e02f918738499d81247c425139f4f
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-04-07 19:19:19 +0530

    Time: 20 ms (92.99%), Space: 28.9 MB (21.33%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -13,15 +13,12 @@ class Solution {
 public:
      bool ans=false;
     bool check(TreeNode *root,TreeNode *subRoot){
-        if(root==NULL && subRoot==NULL)
-            return true;
-       if(subRoot==NULL || root==NULL)
-           return false;
+        if(root==NULL && subRoot==NULL){return true;}
+       if(subRoot==NULL || root==NULL){return false;}
         return (root->val==subRoot->val) && check(root->left,subRoot->left) && check(root->right,subRoot->right);
     }
     void inorder(TreeNode *root,TreeNode *subRoot){
-        if(root==NULL)
-            return;
+        if(root==NULL){ return;}
         inorder(root->left,subRoot);
         if(root->val==subRoot->val){
             TreeNode *temp=root;
