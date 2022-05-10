52655ae41c2dfd04ee1891ac15da9e101dd0b7cf
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-05-10 19:17:04 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (58.80%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,24 +1,29 @@
 class Solution {
+ vector<vector<int>> res;
 public:
-    void solve(int st,int k,int n,vector<int> &ds,vector<vector<int>> &ans)
-    {
-        if(k<=0)
-        {
-            if(n==0) ans.push_back(ds);
-            return;
-        }
-        
-        for(int i=st;i<=9;i++)    
-        {
-            ds.push_back(i);
-            solve(i+1,k-1,n-i,ds,ans);
-            ds.pop_back();
+    void getComb(int k, int n, vector<int> curr, int sum){
+        if(k==0){
+            if(sum == n)
+                res.push_back(curr);
+        } else {
+            for(int i=curr[curr.size()-1]+1;i<=9-k+1;i++){
+                if(sum+i <= n){
+                    curr.push_back(i);
+                    getComb(k-1, n, curr, sum+i);
+                    curr.pop_back();
+                }
+            }
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-       vector<vector<int>> ans;
-        vector<int> ds;
-        solve(1,k,n,ds,ans);
-        return ans;
+        if(n >= (k*(k+1))/2){
+            for(int i=1;i<=9-k+1;i++){
+                vector<int> curr={i};
+                int sum = i;
+                if(sum<=n)
+                    getComb(k-1, n, curr, sum);
+            }
+        }
+        return res;
     }
 };
