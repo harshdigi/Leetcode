e6906f8607b8b753902d3ccf9681d21bfdac5e37
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-05-10 19:22:56 +0530

    Time: 9 ms (6.54%), Space: 13.5 MB (5.83%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,29 +1,25 @@
 class Solution {
- vector<vector<int>> res;
 public:
-    void getComb(int k, int n, vector<int> curr, int sum){
-        if(k==0){
-            if(sum == n)
-                res.push_back(curr);
-        } else {
-            for(int i=curr[curr.size()-1]+1;i<=9-k+1;i++){
-                if(sum+i <= n){
-                    curr.push_back(i);
-                    getComb(k-1, n, curr, sum+i);
-                    curr.pop_back();
-                }
+    vector<vector<int>> res;
+    void backtrack(int n, int cS, int cK,  vector<int> in = {}, int i=1){
+        if(cK ==0 ){
+            if(cS == n){
+            res.push_back(in);
             }
+            return;
         }
+        if(cS > n || i>9){
+            return;
+        }
+        in.push_back(i);
+        cS += i;
+        backtrack(n,cS,cK-1,in, i+1);
+        in.pop_back();
+        backtrack(n,cS-i,cK,in, i+1);        
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        if(n >= (k*(k+1))/2){
-            for(int i=1;i<=9-k+1;i++){
-                vector<int> curr={i};
-                int sum = i;
-                if(sum<=n)
-                    getComb(k-1, n, curr, sum);
-            }
-        }
+        if((k*(k-1))/2 <=n){
+        backtrack(n, 0, k);}
         return res;
     }
 };
