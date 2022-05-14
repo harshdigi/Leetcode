c9815f3494e3e29b854de235f0a485d95765fb49
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-05-14 15:20:17 +0530

    Time: 87 ms (81.64%), Space: 69.5 MB (52.19%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,21 +1,54 @@
 class Solution {
 public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int gSum=0, cSum=0;
-        int  i=0, N = gas.size();
-      int rem=0, ans=0;
-        while(i<N){
-            gSum+=gas[i];
-            cSum+=cost[i];
-            rem+= gas[i]- cost[i];
-            if(rem<0){
-                ans=i+1;
-                rem=0;
-            }
-          
-            i++;
+    int canCompleteCircuit(vector<int>& A, vector<int>& B) {
+         int front = 0, end = 0;
+
+    int N = A.size();
+
+    long s1 = 0, s2 = 0;
+
+    bool endGoneAround = false;
+
+    while (true){
+
+        end++;
+
+        if (end == N){
+
+            end=0;
+
+            endGoneAround = true;
+
         }
-        return  gSum>=cSum?ans:-1;
-            
+
+        int endMinus1 = (end==0) ? N-1: end-1;
+
+        s1+=A[endMinus1];
+
+        s2+=B[endMinus1];
+
+        while (s2>s1 && front!= N){
+
+            front++;
+
+            s1 -= A[front-1];
+
+            s2 -= B[front-1];
+
+        }
+
+        if (front == end && endGoneAround){
+
+            return front;
+
+        }
+
+        if (front == N) return -1;
+
+    }
+
+    return -1;
+
+
     }
 };
