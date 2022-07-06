cd3612f714fc2c9171904f99ad39673c30265f2e
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-07-06 14:06:29 +0530

    Time: 39 ms (49.98%), Space: 32.7 MB (5.50%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        vector<int> prefixSum;
+        vector<int> prefixSum; // array to store prefix sum to find the sum of left and right side elements in the array 
+        // if size of input array is 1 we can simply return index 0 as pivot
         if(nums.size() ==  1){
             return 0;
         }
-        int sum  = nums[0];
+        // prefix sum  array calculation start 
+        int sum  = nums[0]; // this will store the sum of elements
         prefixSum.push_back(sum);
         for(int i=1;i<nums.size();i++){
             sum += nums[i];
             prefixSum.push_back(sum);
         }
+        // prefix sum end
+        
+        // finding pivot index start
         
         for(int i=0;i<nums.size();i++){
-            int left = prefixSum[i] - nums[i];
-            int right = sum - prefixSum[i];
+            int left = prefixSum[i] - nums[i]; // this will calculate the sum of elements on the left side of the current element
+            int right = sum - prefixSum[i]; // this will calculate the sum of elements on the right side of the current element
+            // checking the given condition for the index to be pivot index
             if(left == right){
-                return i;
+                return i; // return current index 'i'  
             }
         }
-        return -1;
+        return -1; // if no such pivotIndex is found we return -1
         
     }
 };
