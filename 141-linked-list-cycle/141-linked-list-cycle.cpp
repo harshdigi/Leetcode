fa8b195193ae0e7608130070e1d54825f0f35f0c
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-07-06 14:22:33 +0530

    Time: 11 ms (83.37%), Space: 8.1 MB (16.61%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,18 +9,26 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* fast = head;
-        ListNode* slow = head;
+        // if given head node is NULL and head->next is NULL, this means that there will be no cycle
         if(head == NULL or head->next == NULL){
             return false;
         }
+        
+        ListNode* fast = head; // fast pointer will take two step at a time
+        ListNode* slow = head; // slow pointer will take one step at a time\
+        
+        // run the loop untill fast reaches to NULL, If null is encountered anywhere in the loop that means list does not contains cycle
         while(fast->next != NULL && fast->next->next != NULL){
-            fast = fast->next->next;
-            slow = slow->next;
+            fast = fast->next->next; // incrementing the fast pointer by two nodes
+            
+            slow = slow->next; // incrementing the slow pointer by one step
+            
+            // slow and fast pointer become at any point this means list contains cycle
             if(slow == fast){
                 return true;
             }
         }
+        // no loop in list
         return false;
     }
     
