fc584ce54c10f95597032f7deb0d8f90f84d010f
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-07-06 12:20:36 +0530

    Time: 8 ms (23.07%), Space: 8.5 MB (34.66%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> runningSum(vector<int>& nums) {
         vector<int> prefixSum; // Output array
         // if size of input array is 0 and 1 then there will be no difference between input array and  output array
-        if(nums.size() == 1 || nums.size() == 0){
+        if(nums.size() == 1 || nums.size() == 0)
             return nums;
-        }
+        
         // setting first elemet equals to sum
         int sum=nums[0];
         // inserting the sum in output array
