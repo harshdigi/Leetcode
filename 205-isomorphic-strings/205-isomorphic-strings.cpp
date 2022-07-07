8ec6a7df1aa8a112111da7f69d62c1b6f05a4d2b
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-07-07 11:49:39 +0530

    Time: 17 ms (21.82%), Space: 7.1 MB (46.29%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         unordered_map<char, int> prevS, prevT;
+        
         for(int i =0 ;i<s.length();i++){
-            int a = s[i];
-            int b= t[i];
-            if(prevS[a] !=0){
-                if(b != t[prevS[a] -1 ]){
+            if(prevS[s[i]] !=0){
+                if(t[i] != t[prevS[s[i]] -1 ]){
                     return false;
                 }
             }
-            else if(prevT[b]!=0){
-                if(a != s[prevT[b]-1]){
+            else if(prevT[t[i]]!=0){
+                if(s[i] != s[prevT[t[i]]]-1){
                     return false;
                 }
             }
-            prevS[a] =i+1;
-            prevT[b] = i+1;
+            prevS[s[i]] =i+1;
+            prevT[t[i]] = i+1;
         }
         
       return true;  
