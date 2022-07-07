ed03842b87ab978c05020c61c60eeb32afc13746
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-07-07 11:45:15 +0530

    Time: 24 ms (8.97%), Space: 7 MB (46.29%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()!= t.length()){
+            return false;
+        }
+        unordered_map<char, int> prevS, prevT;
+        
+        for(int i =0 ;i<s.length();i++){
+            if(prevS[s[i]] !=0){
+                if(t[i] != t[prevS[s[i]] -1 ]){
+                    return false;
+                }
+            }
+            else if(prevT[t[i]]!=0){
+                if(s[i] != s[prevT[t[i]]]-1){
+                    return false;
+                }
+            }
+            prevS[s[i]] =i+1;
+            prevT[t[i]] = i+1;
+        }
+        
+      return true;  
+    }
+};
