18effbc40753365fc82ac2f50bdc5077f97539b4
Harsh Ahuja <43691044+harshdigi@users.noreply.github.com>
2022-10-21 11:23:14 +0530

    Time: 10 ms (18.71%), Space: 8.6 MB (37.05%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> prefixSum; // Output array
-        // if size of input array is 0 and 1 then there will be no difference between input array and  output array
-        if(nums.size() == 1 || nums.size() == 0){
-            return nums;}
-        
-        // setting first elemet equals to sum
-        int sum=nums[0];
-        // inserting the sum in output array
-        prefixSum.push_back(sum);
-        // inserting running sum in output array
-        for(int i=1;i<nums.size();i++){
-            // new sum will the add of current sum + current element in nums array
-            sum = sum+nums[i];
-            prefixSum.push_back(sum);
+        vector<int> res;
+        int sum =0;
+        for(int i =0;i<nums.size();i++){
+            sum += nums[i];
+            res.push_back(sum);
         }
-        return prefixSum;
+        return res;
     }
-    
 };
